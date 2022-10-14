c24c723560d0a06dbcac40e8010aac2321098332
Ludovic Rousselle <lrousselle@artfx.fr>
2022-10-14 10:18:29 +0200

    Full Bugged

--- Assets/PolygonKnights/Script/Mario_Start.cs
+++ Assets/PolygonKnights/Script/Mario_Start.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Mario_Start : MonoBehaviour
 {
 
     [SerializeField] Transform parent;
     [SerializeField] GameObject child;
+    [SerializeField] GameObject triggerZone;
+    [SerializeField] float waitingTime;
+    private bool isStarted = false;
+
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +24,42 @@ public class Mario_Start : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        /* Invoke("TriggerFollow", waitingTime); */
+
+        if (direction.magnitude < 0)
+        {
+            Debug.Log("rikiki");
+            child.transform.SetParent(null);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("wololo");
         child.transform.SetParent(parent);
+        isStarted = true;
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("squalala");
         child.transform.SetParent(null);
+        isStarted = false;
     }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        direction = ctx.ReadValue<Vector2>();
+    }
+
+    /* void TriggerFollow()
+    {
+        if (direction.magnitude > 0 && isStarted == true)
+        {
+            Debug.Log("chalala");
+            triggerZone.transform.SetParent(parent);
+        }
+    } */
 }
